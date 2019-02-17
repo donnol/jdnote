bc548c65001766e55092447f055c486be92b69f8
jdlau <jdlau@qq.com>
2019-02-17 15:20:24 +0800

    utils/log添加logger

--- utils/log/log.go
+++ utils/log/log.go
@@ -2,7 +2,10 @@ package log
 
 import (
 	"fmt"
+	"io"
 	"log"
+	"math/rand"
+	"time"
 )
 
 // 日志级别--https://stackoverflow.com/questions/2031163/when-to-use-the-different-log-levels
@@ -14,6 +17,68 @@ const (
 	DebugLevel = "DEBUG"
 	TraceLevel = "TRACE"
 )
+
+var (
+	logRand *rand.Rand
+)
+
+func init() {
+	// 初始化随机数生成器
+	source := rand.NewSource(time.Now().Unix())
+	logRand = rand.New(source)
+}
+
+// Logger 日志
+type Logger struct {
+	*log.Logger
+}
+
+// New 新建
+func New(out io.Writer, prefix string, flag int) *Logger {
+	return &Logger{
+		Logger: log.New(out, prefix+" ", flag),
+	}
+}
+
+// Fatalf 致命
+func (l *Logger) Fatalf(format string, v ...interface{}) {
+	format = getFormat(FatalLevel, format)
+	l.printf(format, v...)
+}
+
+// Errorf 错误
+func (l *Logger) Errorf(format string, v ...interface{}) {
+	format = getFormat(ErrorLevel, format)
+	l.printf(format, v...)
+}
+
+// Warnf 警告
+func (l *Logger) Warnf(format string, v ...interface{}) {
+	format = getFormat(WarnLevel, format)
+	l.printf(format, v...)
+}
+
+// Infof 信息
+func (l *Logger) Infof(format string, v ...interface{}) {
+	format = getFormat(InfoLevel, format)
+	l.printf(format, v...)
+}
+
+// Debugf 调试
+func (l *Logger) Debugf(format string, v ...interface{}) {
+	format = getFormat(DebugLevel, format)
+	l.printf(format, v...)
+}
+
+// Tracef 追踪
+func (l *Logger) Tracef(format string, v ...interface{}) {
+	format = getFormat(TraceLevel, format)
+	l.printf(format, v...)
+}
+
+func (l *Logger) printf(format string, v ...interface{}) {
+	l.Logger.Output(3, fmt.Sprintf(format, v...))
+}
 
 // Fatalf 致命
 func Fatalf(format string, v ...interface{}) {
@@ -52,9 +117,10 @@ func Tracef(format string, v ...interface{}) {
 }
 
 func getFormat(level, format string) string {
-	return fmt.Sprintf("[%s] %s", level, format)
+	r := logRand.Int63()
+	return fmt.Sprintf("|%d| [%s] %s", r, level, format)
 }
 
 func printf(format string, v ...interface{}) {
-	log.Printf(format, v...)
+	log.Output(3, fmt.Sprintf(format, v...))
 }
--- utils/log/log_test.go
+++ utils/log/log_test.go
@@ -2,10 +2,13 @@ package log
 
 import (
 	"errors"
+	"log"
+	"os"
 	"testing"
 )
 
 func TestInfo(t *testing.T) {
+	log.SetFlags(log.Lshortfile | log.LstdFlags)
 	for _, cas := range []struct {
 		value interface{}
 	}{
@@ -17,5 +20,24 @@ func TestInfo(t *testing.T) {
 		Infof("%+v\n", cas.value)
 		Debugf("%+v\n", cas.value)
 		Tracef("%+v\n", cas.value)
+	}
+}
+
+func TestLogger(t *testing.T) {
+	logger := New(os.Stdout, "[Haha]", log.LstdFlags|log.Lshortfile)
+
+	for _, cas := range []struct {
+		value interface{}
+	}{
+		{errors.New("Test Message")},
+	} {
+		logger.Fatalf("%+v\n", cas.value)
+		logger.Errorf("%+v\n", cas.value)
+		logger.Warnf("%+v\n", cas.value)
+		logger.Infof("%+v\n", cas.value)
+		logger.Debugf("%+v\n", cas.value)
+		logger.Tracef("%+v\n", cas.value)
+
+		logger.Printf("%+v\n", cas.value)
 	}
 }
