ba6c993815fca4fc36a51d58a57701fe717ff7d1
jdlau <jdlau@qq.com>
2019-02-17 15:21:27 +0800

    utils/worker去掉jobs数组

--- utils/worker/worker.go
+++ utils/worker/worker.go
@@ -1,20 +1,76 @@
 package worker
 
 import (
+	"errors"
 	"log"
 	"os"
 	"sync"
+	"time"
+
+	utillog "github.com/donnol/jdnote/utils/log"
 )
 
 const (
 	jobCount = 100
 	errCount = 100
+
+	defaultCount = 50
 )
 
-var logger = log.New(os.Stdout, "[Worker] ", log.LstdFlags|log.Lshortfile)
+var logger = utillog.New(os.Stdout, "[Worker]", log.LstdFlags|log.Lshortfile)
+
+// 错误
+var (
+	ErrWorkerIsStop = errors.New("Worker is stop")
+	ErrNilJobDo     = errors.New("Job do field is nil")
+)
+
+// DefaultWorker 默认Wroker
+var DefaultWorker = New(defaultCount)
+
+func init() {
+	DefaultWorker.Start()
+}
 
 // Job 工作
-type Job func() error
+type Job struct {
+	do Do // 方法
+
+	timeout time.Duration // 超时时间
+
+	errorHandler ErrorHandler // 错误处理方法
+}
+
+// Do 执行
+type Do func() error
+
+// ErrorHandler 错误处理方法
+type ErrorHandler func(error)
+
+// MakeJob 新建工作
+func MakeJob(do Do, timeout time.Duration, eh ErrorHandler) Job {
+	return Job{
+		do:           do,
+		timeout:      timeout,
+		errorHandler: eh,
+	}
+}
+
+func (job Job) run() error {
+	if job.do == nil {
+		return ErrNilJobDo
+	}
+
+	if err := job.do(); err != nil {
+		if job.errorHandler != nil {
+			job.errorHandler(err)
+		} else {
+			return err
+		}
+	}
+
+	return nil
+}
 
 // Worker 工人
 type Worker struct {
@@ -24,22 +80,20 @@ type Worker struct {
 	jobChan   chan Job      // 工作管道
 	errChan   chan error    // 错误管道
 
-	jobs []Job // 工作列表
-
-	wg *sync.WaitGroup
+	wg   *sync.WaitGroup
+	stop bool // 是否调用了Stop方法
 }
 
 // New 新建
 func New(n int) *Worker {
 	if n <= 0 {
-		n = 10
+		n = defaultCount
 	}
 	return &Worker{
 		limitChan: make(chan struct{}, n),
 		stopChan:  make(chan struct{}),
 		jobChan:   make(chan Job),
 		errChan:   make(chan error, errCount),
-		jobs:      make([]Job, 0),
 		wg:        new(sync.WaitGroup),
 	}
 }
@@ -48,6 +102,8 @@ func New(n int) *Worker {
 func (w *Worker) Start() {
 	go w.handleError()
 	go w.start()
+
+	logger.Debugf("Start.\n")
 }
 
 func (w *Worker) start() {
@@ -63,15 +119,6 @@ func (w *Worker) start() {
 		case <-w.stopChan:
 			w.close()
 			return
-
-		default:
-			if len(w.jobs) != 0 {
-				for _, job := range w.jobs {
-					w.do(job)
-				}
-				// 清空
-				w.jobs = make([]Job, 0)
-			}
 		}
 	}
 }
@@ -82,10 +129,10 @@ func (w *Worker) do(job Job) {
 	w.wg.Add(1)
 
 	// 开始工作
-	go func() {
+	go func(job Job) {
 		defer func() {
 			if r := recover(); r != nil {
-				logger.Printf("job: %+v\n", r)
+				logger.Fatalf("job: %+v\n", r)
 			}
 
 			// 释放一个坑
@@ -94,10 +141,43 @@ func (w *Worker) do(job Job) {
 		}()
 
 		// 执行
-		if err := job(); err != nil {
+		if job.timeout > 0 {
+			w.doWithTimeout(job)
+		} else {
+			if err := job.run(); err != nil {
+				w.errChan <- err
+			}
+		}
+	}(job)
+}
+
+func (w *Worker) doWithTimeout(job Job) {
+	var retChan = make(chan struct{})
+	defer close(retChan)
+
+	// 执行
+	go func(retChan chan struct{}) {
+		defer func() {
+			if r := recover(); r != nil {
+				logger.Fatalf("job exec: %+v\n", r)
+			}
+		}()
+
+		if err := job.run(); err != nil {
 			w.errChan <- err
 		}
-	}()
+		retChan <- struct{}{}
+	}(retChan)
+
+	// 超时
+	timer := time.NewTimer(job.timeout)
+	select {
+	case <-retChan:
+		return
+	case t := <-timer.C:
+		logger.Fatalf("job timeout: %+v\n", t)
+		return
+	}
 }
 
 func (w *Worker) handleError() {
@@ -107,23 +187,27 @@ func (w *Worker) handleError() {
 			if !ok {
 				return
 			}
-			logger.Printf("err is %v\n", err)
+			logger.Fatalf("err is %v\n", err)
 		}
 	}
 }
 
 // Stop 停止
 func (w *Worker) Stop() {
+	w.stop = true
+
 	w.wait()
 
 	w.stopChan <- struct{}{}
+
+	logger.Debugf("Stop.\n")
 }
 
 func (w *Worker) close() {
 	// close管道时，有可能panic
 	defer func() {
 		if r := recover(); r != nil {
-			logger.Printf("close: %+v\n", r)
+			logger.Fatalf("close: %+v\n", r)
 		}
 	}()
 
@@ -139,12 +223,15 @@ func (w *Worker) wait() {
 }
 
 // Push 添加
-func (w *Worker) Push(job Job) {
-	// 如果添加的速度超过处理的速度，就会阻塞。这时，先把job存到jobs里
-	select {
-	case w.jobChan <- job:
-	default:
-		w.jobs = append(w.jobs, job)
-		logger.Println("Full load. Append to jobs")
+func (w *Worker) Push(job Job) error {
+	if w.stop {
+		return ErrWorkerIsStop
 	}
+	if job.do == nil {
+		return ErrNilJobDo
+	}
+
+	w.jobChan <- job
+
+	return nil
 }
