fc364b4b542909bf255568f727bc22f351fee6b3
jdlau <jdlau@qq.com>
2019-02-21 21:09:09 +0800

    utils/log - 添加通知

--- utils/log/log.go
+++ utils/log/log.go
@@ -5,17 +5,13 @@ import (
 	"io"
 	"log"
 	"math/rand"
+	"runtime"
 	"time"
 )
 
-// 日志级别--https://stackoverflow.com/questions/2031163/when-to-use-the-different-log-levels
 const (
-	FatalLevel = "FATAL"
-	ErrorLevel = "ERROR"
-	WarnLevel  = "WARN"
-	InfoLevel  = "INFO"
-	DebugLevel = "DEBUG"
-	TraceLevel = "TRACE"
+	calldepth = 3
+	skip      = 5
 )
 
 var (
@@ -28,9 +24,17 @@ func init() {
 	logRand = rand.New(source)
 }
 
+// Notifier 通知接口
+type Notifier interface {
+	Levels() []Level
+	Notify(msg string)
+}
+
 // Logger 日志
 type Logger struct {
 	*log.Logger
+
+	notify Notifier
 }
 
 // New 新建
@@ -40,87 +44,138 @@ func New(out io.Writer, prefix string, flag int) *Logger {
 	}
 }
 
+// SetNotify 设置通知
+func (l *Logger) SetNotify(notify Notifier) {
+	l.notify = notify
+}
+
 // Fatalf 致命
 func (l *Logger) Fatalf(format string, v ...interface{}) {
-	format = getFormat(FatalLevel, format)
-	l.printf(format, v...)
+	level := FatalLevel
+	l.printf(level, format, v...)
 }
 
 // Errorf 错误
 func (l *Logger) Errorf(format string, v ...interface{}) {
-	format = getFormat(ErrorLevel, format)
-	l.printf(format, v...)
+	level := ErrorLevel
+	l.printf(level, format, v...)
 }
 
 // Warnf 警告
 func (l *Logger) Warnf(format string, v ...interface{}) {
-	format = getFormat(WarnLevel, format)
-	l.printf(format, v...)
+	level := WarnLevel
+	l.printf(level, format, v...)
 }
 
 // Infof 信息
 func (l *Logger) Infof(format string, v ...interface{}) {
-	format = getFormat(InfoLevel, format)
-	l.printf(format, v...)
+	level := InfoLevel
+	l.printf(level, format, v...)
 }
 
 // Debugf 调试
 func (l *Logger) Debugf(format string, v ...interface{}) {
-	format = getFormat(DebugLevel, format)
-	l.printf(format, v...)
+	level := DebugLevel
+	l.printf(level, format, v...)
 }
 
 // Tracef 追踪
 func (l *Logger) Tracef(format string, v ...interface{}) {
-	format = getFormat(TraceLevel, format)
-	l.printf(format, v...)
+	level := TraceLevel
+	l.printf(level, format, v...)
 }
 
-func (l *Logger) printf(format string, v ...interface{}) {
-	l.Logger.Output(3, fmt.Sprintf(format, v...))
+func (l *Logger) printf(level Level, format string, v ...interface{}) {
+	format = getFormat(level, format)
+	msg := fmt.Sprintf(format, v...)
+
+	l.Logger.Output(calldepth, msg)
+
+	// 发送通知
+	l.notice(level, msg)
+}
+
+// 发送通知
+func (l *Logger) notice(level Level, msg string) {
+	if l.notify == nil {
+		return
+	}
+
+	levels := l.notify.Levels()
+	if !InLevel(levels, level) {
+		return
+	}
+
+	stack := collectStack()
+	l.notify.Notify(msg + stack)
 }
 
 // Fatalf 致命
 func Fatalf(format string, v ...interface{}) {
-	format = getFormat(FatalLevel, format)
-	printf(format, v...)
+	level := FatalLevel
+	printf(level, format, v...)
 }
 
 // Errorf 错误
 func Errorf(format string, v ...interface{}) {
-	format = getFormat(ErrorLevel, format)
-	printf(format, v...)
+	level := ErrorLevel
+	printf(level, format, v...)
 }
 
 // Warnf 警告
 func Warnf(format string, v ...interface{}) {
-	format = getFormat(WarnLevel, format)
-	printf(format, v...)
+	level := WarnLevel
+	printf(level, format, v...)
 }
 
 // Infof 信息
 func Infof(format string, v ...interface{}) {
-	format = getFormat(InfoLevel, format)
-	printf(format, v...)
+	level := InfoLevel
+	printf(level, format, v...)
 }
 
 // Debugf 调试
 func Debugf(format string, v ...interface{}) {
-	format = getFormat(DebugLevel, format)
-	printf(format, v...)
+	level := DebugLevel
+	printf(level, format, v...)
 }
 
 // Tracef 追踪
 func Tracef(format string, v ...interface{}) {
-	format = getFormat(TraceLevel, format)
-	printf(format, v...)
+	level := TraceLevel
+	printf(level, format, v...)
 }
 
-func getFormat(level, format string) string {
+func getFormat(level Level, format string) string {
 	r := logRand.Int63()
 	return fmt.Sprintf("|%d| [%s] %s", r, level, format)
 }
 
-func printf(format string, v ...interface{}) {
-	log.Output(3, fmt.Sprintf(format, v...))
+func printf(level Level, format string, v ...interface{}) {
+	format = getFormat(level, format)
+	msg := fmt.Sprintf(format, v...)
+
+	log.Output(calldepth, msg)
+}
+
+func collectStack() string {
+	var stack string
+
+	// 收集栈信息
+	var pcs = make([]uintptr, 10)
+	n := runtime.Callers(skip, pcs)
+	if n == 0 {
+		return stack
+	}
+	pcs = pcs[:n]
+	frames := runtime.CallersFrames(pcs)
+	for {
+		next, more := frames.Next()
+		if !more {
+			break
+		}
+		stack += fmt.Sprintf("%v:%v\n%v\n", next.File, next.Line, next.Function)
+	}
+
+	return fmt.Sprintf("\n%s\n", stack)
 }
