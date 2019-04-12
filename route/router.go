bbf9a4a9c0f9fc15f9325f1fff7268f3dcb2ea5f
liujiandong <liujiandong@xiao100.com>
2019-04-12 17:40:07 +0800

    route: 移动方法到新文件

--- route/router.go
+++ route/router.go
@@ -7,20 +7,12 @@ import (
 	"net/http"
 	"os"
 	"reflect"
-	"runtime"
-	"strings"
-	"unicode"
 
 	"github.com/donnol/jdnote/config"
 	pg "github.com/donnol/jdnote/store/db/postgresql"
 	"github.com/donnol/jdnote/utils/jwt"
 	utillog "github.com/donnol/jdnote/utils/log"
 	"github.com/gin-gonic/gin"
-)
-
-// 路径分割符
-const (
-	pathSep = "/"
 )
 
 // cookie相关
@@ -177,75 +169,6 @@ func (r *Router) RegisterStruct(v interface{}) {
 			panic("Not support method now.")
 		}
 	}
-}
-
-func addPathPrefix(path, prefix string) string {
-	return pathSep + strings.ToLower(prefix) + path
-}
-
-// getMethodPathFromFunc 通过f的名字获取method，path
-func getMethodPathFromFunc(f HandlerFunc) (method, path string) {
-	// 利用反射和运行时获取函数名
-	refValue := reflect.ValueOf(f)
-	fn := runtime.FuncForPC(refValue.Pointer())
-	fullFuncName := fn.Name()
-
-	return getMethodPath(fullFuncName)
-}
-
-// getMethodPath 获取methid, path
-func getMethodPath(fullFuncName string) (method, path string) {
-	const sep = "."
-
-	upperFunc := func(r rune) bool {
-		return unicode.IsUpper(r)
-	}
-
-	// 过滤函数名的包名部分
-	lastDotIndex := strings.LastIndex(fullFuncName, sep)
-	funcName := fullFuncName[lastDotIndex+1:]
-
-	// 找到函数名里的首个大写字母，并以此作为依据将字符串分割
-	firstUpperIndex := strings.IndexFunc(funcName, upperFunc)
-	if firstUpperIndex == 0 {
-		// 如果方法是可导出的，首字母就是大写，需要过滤掉
-		firstUpperIndex = strings.IndexFunc(funcName[1:], upperFunc) + 1
-	}
-	method = funcName[:firstUpperIndex]
-	method = methodMap(method)
-
-	// 如果剩下的路径部分还有大写字母，需要分为多段路径
-	tmpPath := funcName[firstUpperIndex:]
-	for {
-		tmpPath = strings.ToLower(tmpPath[:1]) + tmpPath[1:]
-		firstUpperIndex = strings.IndexFunc(tmpPath, upperFunc)
-		if firstUpperIndex == -1 {
-			path += pathSep + strings.ToLower(tmpPath)
-			return
-		}
-		path += pathSep + strings.ToLower(tmpPath[:firstUpperIndex])
-
-		tmpPath = tmpPath[firstUpperIndex:]
-	}
-
-	return
-}
-
-func methodMap(m string) (r string) {
-	m = strings.ToLower(m)
-	switch m {
-	case "get":
-		r = http.MethodGet
-	case "add":
-		r = http.MethodPost
-	case "mod":
-		r = http.MethodPut
-	case "del":
-		r = http.MethodDelete
-	default:
-		r = http.MethodPost
-	}
-	return
 }
 
 var structHandlerFunc = func(method string, f HandlerFunc) gin.HandlerFunc {
