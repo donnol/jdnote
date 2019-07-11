0a21c8f19398cf9a5fa0b72a5213bfae4776fcd7
jdlau <jdlau@qq.com>
2019-07-11 21:31:08 +0800

    model: 给表结构体添加rel标签

--- model/note/entity.go
+++ model/note/entity.go
@@ -4,9 +4,9 @@ import "time"
 
 // Entity 笔记
 type Entity struct {
-	ID        int       `json:"id"`                        // 记录ID
-	UserID    int       `json:"userID" db:"user_id"`       // 用户ID
-	Title     string    `json:"title"`                     // 标题
-	Detail    string    `json:"detail"`                    // 详情
-	CreatedAt time.Time `json:"createdAt" db:"created_at"` // 创建时间
+	ID        int       `json:"id"`                                  // 记录ID
+	UserID    int       `json:"userID" db:"user_id" rel:"t_user.id"` // 用户ID
+	Title     string    `json:"title"`                               // 标题
+	Detail    string    `json:"detail"`                              // 详情
+	CreatedAt time.Time `json:"createdAt" db:"created_at"`           // 创建时间
 }
--- model/user_role/entity.go
+++ model/user_role/entity.go
@@ -2,7 +2,7 @@ package userrole
 
 // Entity 实体-对应数据库表
 type Entity struct {
-	ID     int `json:"id"`                  // 记录ID
-	UserID int `json:"userID" db:"user_id"` // 用户ID
-	RoleID int `json:"roleID" db:"role_id"` // 角色ID
+	ID     int `json:"id"`                                  // 记录ID
+	UserID int `json:"userID" db:"user_id" rel:"t_user.id"` // 用户ID
+	RoleID int `json:"roleID" db:"role_id" rel:"t_role.id"` // 角色ID
 }
