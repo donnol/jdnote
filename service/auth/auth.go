a62782929ec163f06fa3d13115996fc1c7f921b3
jdlau <jdlau@qq.com>
2019-07-15 21:55:33 +0800

    service/auth: 添加校验

--- service/auth/auth.go
+++ service/auth/auth.go
@@ -12,6 +12,9 @@ type Auth struct {
 
 // CheckPerm 检查用户是否拥有指定权限
 func (a *Auth) CheckPerm(ctx context.Context, perms []string) error {
+	if err := a.RoleActionModel.CheckPerm(ctx, perms); err != nil {
+		return err
+	}
 
 	return nil
 }
