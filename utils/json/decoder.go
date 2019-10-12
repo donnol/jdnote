10112dd3de4b3c55fb2149fd083d55ccab0ba3e3
liujiandong <liujiandong@xiao100.com>
2019-10-12 12:02:10 +0800

    utils/json/: 添加coder

---
+++ utils/json/decoder.go
@@ -0,0 +1,6 @@
+package json
+
+// Decoder 解码
+// 将字段映射到指定结构的同名导出字段
+type Decoder struct {
+}
---
+++ utils/json/encoder.go
@@ -0,0 +1,7 @@
+package json
+
+// Encoder 加密 TODO:
+// 默认将导出字段的首字母改为小写
+// 避免在定义结构体时写json tag，减少工作量
+type Encoder struct {
+}
