1a4eef64148f28b1e031ee7c0406360309027275
jdlau <jdlau@qq.com>
2019-12-15 11:38:31 +0800

    utils/: 添加cache

---
+++ utils/cache/cache_test.go
@@ -0,0 +1,58 @@
+package cache
+
+import (
+	"reflect"
+	"testing"
+	"time"
+)
+
+func TestMemImpl(t *testing.T) {
+	opt := Option{
+		Type: TypeMem,
+	}
+	c := New(opt)
+
+	for _, cas := range []struct {
+		key   string
+		value interface{}
+	}{
+		{"key1", "value1"},
+	} {
+		r := c.Set(cas.key, cas.value)
+		if !r {
+			t.Fatalf("Set failed: %v\n", r)
+		}
+
+		v := c.Get(cas.key)
+		if !reflect.DeepEqual(v, cas.value) {
+			t.Fatalf("Get failed: %v != %v\n", v, cas.value)
+		}
+
+		v, ok := c.Lookup(cas.key)
+		if !ok {
+			t.Fatalf("Lookup failed: %v\n", ok)
+		}
+		if !reflect.DeepEqual(v, cas.value) {
+			t.Fatalf("Lookup failed: %v != %v\n", v, cas.value)
+		}
+
+		n := time.Duration(5)
+		expire := time.Second * n
+		r = c.SetNX(cas.key, cas.value, expire)
+		if !r {
+			t.Fatalf("Setnx failed: %v\n", r)
+		}
+		// 5秒内获取
+		v = c.Get(cas.key)
+		if !reflect.DeepEqual(v, cas.value) {
+			t.Fatalf("Get failed: %v != %v\n", v, cas.value)
+		}
+
+		// 5秒后获取
+		time.Sleep(time.Second * (n + 1))
+		v = c.Get(cas.key)
+		if v != nil {
+			t.Fatalf("Get failed: %v != %v\n", v, cas.value)
+		}
+	}
+}
