bf76e2b1738d3d9c02dc522468799e140b23bbcb
donnol <donnol@outlook.com>
2020-12-20 19:18:04 +0800

    添加hook

--- app/register/hook.go
+++ app/register/hook.go
@@ -3,6 +3,7 @@ package register
 import (
 	"reflect"
 
+	"github.com/donnol/jdnote/models/notemodel"
 	"github.com/donnol/jdnote/stores/notestore"
 	"github.com/donnol/jdnote/stores/userstore"
 
@@ -12,7 +13,7 @@ import (
 var (
 	// pctx需要执行的特别函数
 	// 在这里，可以对method, args, result做手脚，但是均不建议这样做
-	// 所以最好是只在方法调用前后做一些操作
+	// 推荐做法是只在方法调用前后做一些操作
 	ArounderMap = map[inject.ProxyContext]inject.AroundFunc{
 		userstore.UserMockGetByIDProxyContext: func(pctx inject.ProxyContext, method reflect.Value, args []reflect.Value) []reflect.Value {
 			var result []reflect.Value
@@ -31,6 +32,40 @@ var (
 			// 打印参数
 			for _, arg := range args {
 				if arg.CanInterface() {
+					pctx.Logf("arg: %v, %s\n", arg.Interface(), arg.String())
+				} else {
+					pctx.Logf("arg: %v, %s\n", arg, arg.String())
+				}
+			}
+
+			result = method.Call(args)
+
+			// 打印结果
+			for _, r := range result {
+				if r.CanInterface() {
+					pctx.Logf("result: %v, %s\n", r.Interface(), r.String())
+				} else {
+					pctx.Logf("result: %v, %s\n", r, r.String())
+				}
+			}
+
+			return result
+		},
+		notestore.NoterMockModProxyContext: func(pctx inject.ProxyContext, method reflect.Value, args []reflect.Value) []reflect.Value {
+			var result []reflect.Value
+
+			// 尝试修改参数里的标题
+			for i, arg := range args {
+				if arg.CanInterface() {
+					// 以参数位置和参数类型判断
+					// 类型是 notemodel.Entity
+					paramType := reflect.TypeOf((*notemodel.Entity)(nil)).Elem()
+					argType := arg.Type()
+					if i == 2 && argType == paramType {
+						// reflectx.SetStructFieldValue(arg, fieldName, value)
+
+						pctx.Logf("arg notemodel.Entity: %v, %s\n", arg.Interface(), arg.String())
+					}
 					pctx.Logf("arg: %v, %s\n", arg.Interface(), arg.String())
 				} else {
 					pctx.Logf("arg: %v, %s\n", arg, arg.String())
